e9cf4aa74867e0ba0c0e6b83c314d229d33ae58d
Alex Machado <amachado@sva.edu>
2021-07-04 16:12:21 -0400

    Pass data to Tracks.js

--- src/components/Tracks/Tracks.js
+++ src/components/Tracks/Tracks.js
@@ -2,27 +2,30 @@ import React from 'react';
 import './Tracks.css';
 
 const Tracks = (props) => {
+    
+    const trackMap = props.tracks.map(track => {
+        return <div key={track.id} className="track">
+        <div className="track-artwork">
+            <img src={track.album.images[1].url} alt="album" />
+        </div>
+        <div className="track-datapoints">
+            <p>artist</p>
+            <p>title</p>
+            <p>album</p>
+        </div>
+        <div className="track-info">
+            <p><b>{track.artists[0].name}</b></p>
+            <p>{track.name}</p>
+            <p>{track.album.name}</p>
+        </div>
+    </div>
+    })
+    
     return(
         <div>
             <h3>your music</h3>
             <div className="music-area">
-                <div className="track">
-                    <div className="track-artwork">
-                        <img src="https://i.scdn.co/image/ab67616d00001e020f7ad6d8d829906c17cae210" alt="album" />
-                    </div>
-                    <div className="track-datapoints">
-                        <p>artist</p>
-                        <p>title</p>
-                        <p>album</p>
-                        <p>vibe</p>
-                    </div>
-                    <div className="track-info">
-                        <p><b>dolly parton</b></p>
-                        <p>city girl</p>
-                        <p>here you come again</p>
-                        <p>country</p>
-                    </div>
-                </div>
+                {trackMap}
             </div>
         </div>
     )
