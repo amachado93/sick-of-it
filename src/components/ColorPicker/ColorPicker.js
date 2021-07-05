92768b8ed00252ba275ed420b9a1329cc937c416
Alex Machado <amachado@sva.edu>
2021-07-05 15:15:02 -0400

    Add styling to ColorPicker.js

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -1,62 +1,26 @@
 import React from 'react';
 
-const ColorPicker = (props) => {
+import './ColorPicker.css';
 
-    // 9 key/value pairs for colors and corresponding tempo
-    const colorData = [
-        {
-            color: 'red',
-            bpm: 70
-        },
-        {
-            color: 'orange',
-            bpm: 95
-        },
-        {
-            color: 'yellow',
-            bpm: 120
-        },
-        {
-            color: 'yellow-green',
-            bpm: 135
-        },
-        {
-            color: 'green',
-            bpm: 155
-        },
-        {
-            color: 'teal',
-            bpm: 165
-        },
-        {
-            color: 'blue',
-            bpm: 180
-        },
-        {
-            color: 'indigo',
-            bpm: 200
-        },
-        {
-            color: 'violet',
-            bpm: 220
-        }
-    ];
+import { ColorData } from './ColorData';
+
+const ColorPicker = (props) => {
     
     const handleClick = e => {
         props.colorChoice(e.target.className)
     }
 
-    const colorSquares = colorData.map( color => {
+    const colorSquares = ColorData.map( color => {
        return <div key={color.bpm} className={color.bpm} onClick={handleClick}>
-                {color.color}
+                <p>{color.color}</p>
             </div>
         }
     )
 
     return(
-        <div>
+        <div className="color-picker">
             <h3>select a color</h3>
-            <div className="colorArea">
+            <div className="color-area">
                 {colorSquares}
             </div>
         </div>
