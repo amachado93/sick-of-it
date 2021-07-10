1c0eb80678da9c1de255a6d36762404e78ad3708
Alex Machado <amachado@sva.edu>
2021-07-10 11:58:28 -0400

    Use dotenv for keys

--- src/App.js
+++ src/App.js
@@ -10,47 +10,46 @@ import Tracks from './components/Tracks/Tracks';
 import Header from './components/Header/Header';
 
 function App() {
-  
+	require('dotenv').config();
 
-  // Set up states for retrieving access token and top tracks
+	// Set up states for retrieving access token and top tracks
 	const [token, setToken] = useState('');
 	const [tracks, setTracks] = useState([]);
 
-  // State for choosing a color
-  const [color, setColor] = useState('');
+	// State for choosing a color
+	const [color, setColor] = useState('');
 
-  // State for choosing an emoji
-  const [emoji, setEmoji] = useState('');
+	// State for choosing an emoji
+	const [emoji, setEmoji] = useState('');
 
-  // variables for api call
-  const genre = emoji;
-  const tempo = color;
+	// variables for api call
+	const genre = emoji;
+	const tempo = color;
 
-  useEffect(()=>{
+	useEffect(()=>{
 
 		// Api call for retrieving token
 		axios('https://accounts.spotify.com/api/token', {
 			'method': 'POST',
 			'headers': {
 				 'Content-Type':'application/x-www-form-urlencoded',
-				 'Authorization': 'Basic ' + (Buffer.from('47121a7fd2fb4c1bae39b1fe32d26270' + ':' + '56586566642040fe8f71b83a6be94ce6').toString('base64')),
+				 'Authorization': 'Basic ' + (Buffer.from(process.env.REACT_APP_SPOTIFY_KEY + ':' + process.env.REACT_APP_SPOTIFY_SECRET).toString('base64')),
 			},
 			data: 'grant_type=client_credentials'
-		}).then(tokenresponse => {
-			console.log(tokenresponse.data.access_token);
-			setToken(tokenresponse.data.access_token);
+		}).then(tokenResponse => {
+			setToken(tokenResponse.data.access_token);
 
 			// Api call for retrieving tracks data
-			axios(`https://api.spotify.com/v1/recommendations?limit=10&market=US&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=${genre}&seed_tracks=0c6xIDDpzE81m2q797ordA&max_tempo=${tempo}`,{
+			axios(`https://api.spotify.com/v1/recommendations?limit=12&market=US&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=${genre}&seed_tracks=0c6xIDDpzE81m2q797ordA&max_tempo=${tempo}`,{
 				'method': 'GET',
 				'headers': {
 					'Content-Type': 'application/json',
 					'Accept': 'application/json',
-					'Authorization': 'Bearer ' + tokenresponse.data.access_token
+					'Authorization': 'Bearer ' + tokenResponse.data.access_token
 				}
-			}).then(trackresponse=> {
-				console.log(trackresponse.data.tracks);
-				setTracks(trackresponse.data.tracks);
+			}).then(trackResponse=> {
+				console.log(trackResponse.data.tracks);
+				setTracks(trackResponse.data.tracks);
 			}).catch(error=> console.log(error))
 		}).catch(error => console.log(error));
 	}, [emoji])
